73e1ed649530930072faed16a44546e7841a1dc3
Yaroslav Shevchenko <yarben03@gmail.com>
2026-05-01 21:55:22 -0400

    Defined 9 fields; created get_key() method; created print method that formats the course information

---
+++ schedule_item.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+@dataclass
+class ScheduleItem:
+    # 1. Define the fields and their data types
+    subject: str
+    catalog: str
+    section: str
+    component: str
+    session: str
+    units: int
+    tot_enrl: int
+    cap_enrl: int
+    instructor: str
+
+    # 2 Implement the get_key() method
+    def get_key(self) -> str:
+        return f"{self.subject}_{self.catalog}_{self.section}"
+
+    # 3. Implement the print() method
+    def print(self):
+        print(f"{self.subject:<8} {self.catalog:<8} {self.section:<8} {self.component:<10} {self.session:<8} {self.units:<7} {self.tot_enrl:<8} {self.cap_enrl:<8} {self.instructor}")
